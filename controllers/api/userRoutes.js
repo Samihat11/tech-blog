c7326a45fdc504f26bf0739c91138755a71e9f4c
Samihat <sameerah11@hotmail.com>
2021-12-04 18:21:10 -0500

    updating username to name

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -5,7 +5,7 @@ const { User } = require('../../models');
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create({
-      username: req.body.username,
+      name: req.body.name,
       email: req.body.email,
       password: req.body.password,
     });
@@ -24,13 +24,13 @@ router.post('/', async (req, res) => {
 //Login
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { username: req.body.username } });
+    const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
       res
         .status(400)
-        .json({ message: 'Incorrect username or password, please try again' });
-        console.log("username is incorrect")
+        .json({ message: 'Incorrect email or password, please try again' });
+        console.log("Email is incorrect")
       return;
     }
 
