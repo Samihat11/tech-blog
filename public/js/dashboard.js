856b0378893c159cc7257d20b1e9f0bcd3080a2e
Samihat <sameerah11@hotmail.com>
2021-12-11 01:04:31 -0500

    added api fetch routes to delete and edit a post

---
+++ public/js/dashboard.js
@@ -0,0 +1,50 @@
+const deleteButtonHandler = async (event) => {
+  event.preventDefault();
+
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+    console.log(id);
+    const response = await fetch(`/api/post/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      location.reload();
+    } else {
+      alert('Failed to delete project');
+    }
+  }
+};
+const editFormHandler = async (e) => {
+  e.preventDefault();
+
+  const title = document.querySelector('#post-title').value.trim();
+  const post_content = document.querySelector('#content').value.trim();
+
+  console.log(title, post_content);
+
+  const url = new URL(document.location);
+  const post_id = url.pathname.split('/').pop();
+
+  if (title && post_content) {
+    const response = await fetch(`/api/post/update/${post_id}`, {
+      method: 'PUT',
+      body: JSON.stringify({ title, post_content }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to create project');
+    }
+  }
+};
+
+document.querySelectorAll('#delete').forEach((deleteButton) => {
+  deleteButton.addEventListener('click', deleteButtonHandler);
+});
+
+document.querySelector('#edit').addEventListener('submit', editFormHandler);
