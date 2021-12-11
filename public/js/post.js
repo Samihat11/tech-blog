350c9ddc76801390df4726621e823d6e5895c3bb
Samihat <sameerah11@hotmail.com>
2021-12-11 01:05:46 -0500

    added fetch route to post a new form

---
+++ public/js/post.js
@@ -0,0 +1,24 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('#post-title').value.trim();
+  const post_content = document.querySelector('#content').value.trim();
+
+  if (title && post_content) {
+    const response = await fetch(`/api/post/new-post`, {
+      method: 'POST',
+      body: JSON.stringify({ title, post_content }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to create project');
+    }
+  }
+};
+
+document.querySelector('#new-post').addEventListener('submit', newFormHandler);
