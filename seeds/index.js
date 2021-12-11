a53c3800f12ee99dac93c5880327d83b22c70387
Samihat <sameerah11@hotmail.com>
2021-12-11 01:00:34 -0500

    adding try catch

--- seeds/index.js
+++ seeds/index.js
@@ -6,27 +6,24 @@ const sequelize = require('../config/connection');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
-    console.log('\n----- DATABASE SYNCED -----\n');
-  try{
-  await seedUsers();
+  console.log('\n----- DATABASE SYNCED -----\n');
+  try {
+    await seedUsers();
     console.log('\n----- USERS SEEDED -----\n');
+  } catch (err) {
+    console.log(err);
   }
-  catch(err){
-    console.log(err)
-  }
-  try{
-  await seedPosts();
+  try {
+    await seedPosts();
     console.log('\n----- POSTS SEEDED -----\n');
+  } catch (err) {
+    console.log(err);
   }
-  catch(err){
-    console.log(err)
-  }
-  try{
-  await seedComments();
+  try {
+    await seedComments();
     console.log('\n----- COMMENTS SEEDED -----\n');
-  }
-  catch(err){
-    console.log(err)
+  } catch (err) {
+    console.log(err);
   }
 
   process.exit(0);
